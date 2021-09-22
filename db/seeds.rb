ad7656bd23852dd3ca74444e84286ef7bfc66091
Mathilde Robert <m.robert@skema.edu>
2021-09-22 15:16:30 +0200

    adapt the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,44 +16,21 @@ SeenResource.destroy_all
 ScopeUser.destroy_all
 
 # 2. Create the scopes
-
-scope1 = Scope.create!(name: 'BNP Paribas')
-scope2 = Scope.create!(name: 'Crédit Agricole')
+Scope.create!(name: 'BNP Paribas 2021')
+Scope.create!(name: 'Crédit Agricole 2021')
 
 # 3. Create the resources
-resource1 = Resource.create!(name: 'Webinar diversité et inclusion 1')
-resource2 = Resource.create!(name: 'Webinar diversité et inclusion 2')
-resource3 = Resource.create!(name: 'Webinar diversité et inclusion 3')
-resource4 = Resource.create!(name: 'Webinar diversité et inclusion 4')
-resource5 = Resource.create!(name: 'Webinar diversité et inclusion 5')
-resource6 = Resource.create!(name: 'Webinar diversité et inclusion 6')
-resource7 = Resource.create!(name: 'Webinar diversité et inclusion 7')
-resource8 = Resource.create!(name: 'Webinar diversité et inclusion 8')
-resource9 = Resource.create!(name: 'Webinar diversité et inclusion 9')
-resource10 = Resource.create!(name: 'Webinar diversité et inclusion 10')
+10.times { |i| Resource.create!(name: "Webinar diversité et inclusion #{i + 1}") }
 
 # 4. Create the users
-user1 = User.create!(email: 'email@user1.com')
-user2 = User.create!(email: 'email@user2.com')
-user3 = User.create!(email: 'email@user3.com')
-user4 = User.create!(email: 'email@user4.com')
-user5 = User.create!(email: 'email@user5.com')
+5.times { |i| User.create!(email: "email@user#{i + 1}.com") }
 
 # 5. Create the seen_resources
-seen_resource1 = SeenResource.create!(user_id: user1.id, resource_id: resource10.id, count: 3)
-seen_resource2 = SeenResource.create!(user_id: user2.id, resource_id: resource9.id, count: 2)
-seen_resource3 = SeenResource.create!(user_id: user3.id, resource_id: resource8.id, count: 1)
-seen_resource4 = SeenResource.create!(user_id: user4.id, resource_id: resource7.id, count: 2)
-seen_resource5 = SeenResource.create!(user_id: user5.id, resource_id: resource4.id, count: 7)
-seen_resource6 = SeenResource.create!(user_id: user1.id, resource_id: resource2.id, count: 2)
-seen_resource7 = SeenResource.create!(user_id: user2.id, resource_id: resource4.id, count: 3)
-seen_resource8 = SeenResource.create!(user_id: user3.id, resource_id: resource7.id, count: 2)
-seen_resource9 = SeenResource.create!(user_id: user4.id, resource_id: resource3.id, count: 1)
-seen_resource10 = SeenResource.create!(user_id: user5.id, resource_id: resource9.id, count: 3)
+Array.new(2) do
+  User.all.find_each { |user| SeenResource.create!(user_id: user.id, resource_id: Resource.all.sample.id, count: 1 + rand(3))}
+end
 
 # 6. Create the scope_users
-scope_user_1 = ScopeUser.create!(scope_id: scope1.id, user_id: user1.id)
-scope_user_2 = ScopeUser.create!(scope_id: scope2.id, user_id: user2.id)
-scope_user_3 = ScopeUser.create!(scope_id: scope1.id, user_id: user3.id)
-scope_user_4 = ScopeUser.create!(scope_id: scope2.id, user_id: user4.id)
-scope_user_5 = ScopeUser.create!(scope_id: scope1.id, user_id: user5.id)
+Array.new(2) do
+  User.all.find_each { |user| ScopeUser.create!(scope_id: Scope.all.sample.id, user_id: user.id) }
+end
